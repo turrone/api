29703cc31e05df3c8187dd1840c273b948dc9df6
Jonathan Hart <stuajnht@users.noreply.github.com>
2019-02-02 22:11:16 +0000

    Structured tests for the API endpoints in relevant directories

--- test/api/api.js
+++ test/api/api.js
@@ -1,29 +1,12 @@
 let chai = require("chai");
 let chaiHttp = require("chai-http");
-let server = require("../src/turrone-server");
+let server = require("../../src/turrone-server");
 let should = chai.should();
-const apiRoot = "/api/turrone/v1/server";
+const apiRoot = "/api";
 
 chai.use(chaiHttp);
 
 describe(apiRoot, () => {
-  /*
-   * Test the /ping [GET] route
-   */
-  describe(`/ping [GET]`, () => {
-    it("it should be alive", done => {
-      chai
-        .request(server)
-        .get(`${apiRoot}/ping`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.an("object");
-          res.body.should.have.property("message").eql("pong");
-          done();
-        });
-    });
-  });
-
   /*
    * Test the /* [GET] route
    */
