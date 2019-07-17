0a9ba8ecbb3c4587788452d1e9af4d0d7a2f96bb
Jonathan Hart <stuajnht@users.noreply.github.com>
2019-07-17 10:21:28 +0100

    Regenerated documentation using updated packages

--- doc/api/api_project.js
+++ doc/api/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-05-18T22:12:05.864Z",
+    "time": "2019-07-17T09:19:55.995Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
